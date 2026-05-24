742212c8e9e775ccb947e1af88f95646e5aec503
Giovanna Karolline <mennezes.giovanna@gmail.com>
2026-05-24 17:59:54 -0300

    criando a função de busca por informações

---
+++ Jogo/buscador_naruto.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_btn_buscar_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	pass # Replace with function body.
